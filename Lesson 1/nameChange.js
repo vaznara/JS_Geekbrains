1ec7f04769c08fe6e54422dcaa5c15f8e4384f9b
Vaznara <vaznara@gmail.com>
2019-04-22 23:06:25 +0400

    ДЗ 1 урока по JS.

---
+++ Lesson 1/nameChange.js
@@ -0,0 +1,6 @@
+var admin;
+var name = 'Василий';
+
+admin = name;
+
+alert('Значение переменной admin: ' + admin);
---
+++ Lesson 1/tempConvert.js
@@ -0,0 +1,4 @@
+var tempCelsius =  prompt('Введите температуру по Цельсию: ');
+var tempFahrenhetit = (9 / 5) * tempCelsius + 32;
+
+alert(tempCelsius + ' Цельсий (°C) = ' + tempFahrenhetit + ' Фаренгейт (°F)')
